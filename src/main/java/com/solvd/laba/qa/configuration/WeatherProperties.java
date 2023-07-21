77258511c8febb1a2be4f51d33e67bde21451d4f
nislam2664 <taranee567@gmail.com>
2023-07-21 09:16:17 -0400

    Added weather properties configuration

---
+++ src/main/java/com/solvd/laba/qa/configuration/WeatherProperties.java
@@ -0,0 +1,34 @@
+package com.solvd.laba.qa.configuration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.util.Properties;
+
+public class WeatherProperties {
+    private static Properties properties;
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    static {
+        initialize();
+    }
+
+    public static void initialize() {
+        properties = new Properties();
+        String weatherFile = "src/test/resources/api_weather/weather.properties";
+
+        try (FileInputStream fis = new FileInputStream(weatherFile)) {
+            properties.load(fis);
+        } catch (IOException e) {
+            LOGGER.error("Could not find " + weatherFile + " ...");
+            LOGGER.error(e.toString());
+        }
+    }
+
+    public static Properties getProperties() {
+        return properties;
+    }
+}
